0b14a679197bf60031cdd8e92c64edf02476548b
Elodie950 <elodie950@gmail.com>
2014-10-14 16:36:30 +0200

    mis à jour

---
+++ MVCMedicoSoft/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using DAL;
+using MVCMedicoSoft.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMedicoSoft.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        [HttpGet]
+        public ActionResult Forms()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Forms(string txtLogin, string txtPassword)
+        {
+            Utilisateur U = Utilisateur.AuthentifieMoi(txtLogin, txtPassword);
+                if(U == null)
+                {
+                    ViewBag.Error = "Try again";
+                    return View();
+                }
+                else{
+                    //1 - stocker en session le login de l'utilisateur
+                    MySession.Login = U.Login;
+                    MySession.User = U;
+                    //2 - rediriger vers Home/Index
+                    return RedirectToRoute(new { Controller="Home", action="Index"});
+                }
+
+        }
+
+        public RedirectToRouteResult LogOut()
+        {
+            MySession.Login= null;
+            Session.Abandon();
+
+            return RedirectToRoute
+                (
+                    new
+                    {
+                        Controller = "Home",
+                        action = "Index"
+                    }
+                );
+
+
+        }
+	}
+}
---
+++ MVCMedicoSoft/Controllers/UserController.cs
@@ -0,0 +1,82 @@
+﻿using DAL;
+using MVCMedicoSoft.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMedicoSoft.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+         [HttpGet]
+        public ActionResult AfficherUsers()
+        {
+            if (MySession.Login == null) return RedirectToRoute(new { controller = "Home", action = "Index" });
+            List<Utilisateur> lu = Utilisateur.getInfos();
+
+            //if (MySession.User.getRole() == TypeOfUser.Medecin)
+            //    MySession.isMedecin = true;
+            //else {
+            //    MySession.isMedecin = false;
+            //} OU
+            MySession.isMedecin = MySession.User.getRole() == TypeOfUser.Medecin;
+
+            return View(lu);
+             /*
+            if (MySession.Login == null)
+            {
+                return RedirectToRoute(new { Controller = "Login", action = "Forms" });
+            }
+            else
+            {
+                List<Utilisateur> maListe = Utilisateur.getInfos();
+                return View(maListe);
+            }*/
+            
+        }
+
+        
+        
+         public ActionResult DetailsUser(int id)
+         {
+             Utilisateur u = Utilisateur.getInfo(id);
+             
+             return View(u as Personne);
+         }
+
+        
+        
+        [HttpGet]
+         public ActionResult EditUser(string id)
+         {
+                 Personne p = Personne.getInfo(id);
+                 return View(p);
+         }
+
+        
+        
+        [HttpPost]
+        public ActionResult EditUser(Personne p)
+        {
+            p.saveMe();
+            return View(p);
+        }
+
+
+
+        public ActionResult AfficherPatient()
+        {
+            //Si on n'est pas loggué, on redirige vers la home
+            if (MySession.Login == null) return RedirectToRoute(
+                    new { controller = "Home", action = "Index" });
+            Medecin me = MySession.User.ConvertMedecin();
+
+            return View(me.getListePatient());
+ 
+        }
+	}
+}
